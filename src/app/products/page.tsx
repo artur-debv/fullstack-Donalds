b62a40552e1bcf216b6411bfc7b38494af80eb79
Ranilo <12ranilo3@gmail.com>
2025-02-19 12:24:06 -0300

    feat: add db seed script

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return <div className="p-5 border border-red-500 rounded-r-xl">
+    <h1 className="text-red-500">Hello, World</h1>
+    <Button>RL</Button>
+    <Input placeholder="Bora começar"/>
+  </div>;
+};
+
+export default ProductPage;
