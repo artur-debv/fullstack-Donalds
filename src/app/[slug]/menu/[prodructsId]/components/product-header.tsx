90b2144eb793f92b8c274e03257b6d3f515e428e
Ranilo <12ranilo3@gmail.com>
2025-02-21 23:22:46 -0300

    feat: add scroll to product details page

--- src/app/[slug]/menu/[prodructsId]/components/product-header.tsx
+++ src/app/[slug]/menu/[prodructsId]/components/product-header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Product } from "@prisma/client";
@@ -7,35 +7,37 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface ProductHeaderPros {
-    product: Pick<Product, 'name' | 'imageUrl'>
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
 const ProductHeader = ({ product }: ProductHeaderPros) => {
-    const router = useRouter()
-    const handleBackClick = () => router.back()
+  const router = useRouter();
+  const handleBackClick = () => router.back();
 
-    return <div className="relative h-[300px] w-full">
-    <div className="relative h-[250px] w-full">
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute left-4 top-4 z-50 rounded-full"
-        onClick={handleBackClick}
-      >
-        <ChevronLeftIcon />
-      </Button>
+  return (
+    <div className="relative min-h-[300px] w-full">
+      <div className="relative h-[250px] w-full">
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute left-4 top-4 z-50 rounded-full"
+          onClick={handleBackClick}
+        >
+          <ChevronLeftIcon />
+        </Button>
 
-      <Image src={product.imageUrl} alt={product.name} fill />
-      
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute right-4 top-4 z-50 rounded-full"
-      >
-        <ScrollTextIcon />
-      </Button>
+        <Image src={product.imageUrl} alt={product.name} fill className="object-contain"/>
+
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute right-4 top-4 z-50 rounded-full"
+        >
+          <ScrollTextIcon />
+        </Button>
+      </div>
     </div>
-  </div>;
-}
- 
+  );
+};
+
 export default ProductHeader;
